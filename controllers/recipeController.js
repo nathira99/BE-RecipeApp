db670e5039e2b688115711042d54d0e0eab719c6
nathira99 <nathirafarveen99@gmail.com>
2025-10-01 15:34:48 +0530

    get all recipes --done

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,11 +2,9 @@ const recipes = require("../models/recipes");
 
 const createRecipe = async (req, res) => {
   try {
-
     const newRecipe = new recipes(req.body);
     const savedRecipe = await newRecipe.save();
-    res.status(201).json({message: 'Recipe created', recipes: savedRecipe})
-
+    res.status(201).json({ message: "Recipe created", recipes: savedRecipe });
   } catch (error) {
     res
       .status(500)
@@ -14,10 +12,15 @@ const createRecipe = async (req, res) => {
   }
 };
 
-const getAllRecipes = (req, res) => {
-  res.json({
-    message: "Get all recipes",
-  });
+const getAllRecipes = async (req, res) => {
+  try {
+    const recipe = await recipes.find().select("-__v");
+    res.status(200).json({ recipe });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Fetching recipe failed...", error: error.message });
+  }
 };
 
 module.exports = {
