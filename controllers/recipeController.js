eca0bb768680094c98cd85f72d8441a6b79a7cce
nathira99 <nathirafarveen99@gmail.com>
2025-10-01 16:36:19 +0530

    delete recipe by id --done

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -41,11 +41,32 @@ const updateRecipeByID = async (req, res) => {
   try {
     const id = req.params.id;
     const updateData = req.body;
-    const updatedRecipe = await recipes.findByIdAndUpdate(id, updateData, { new: true })
-    if(!updatedRecipe){
+    const updatedRecipe = await recipes.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+    if (!updatedRecipe) {
       return res.status(404).json({ message: "Recipe not updated,.." });
-    }else
-    res.status(200).json({ message: "recipe updated"});
+    } else res.status(200).json({ message: "recipe updated" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Updating recipe failed...", error: error.message });
+  }
+};
+
+const deleteRecipeByID = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const deletedRecipe = await recipes.findByIdAndDelete(id);
+
+    if (!deletedRecipe) {
+
+      return res.status(404).json({ message: "Recipe not found..." });
+
+  } else {
+    res.status(200).json({ message: "recipe deleted", Recipe: deletedRecipe});
+  }
   } catch (error) {
     res
       .status(500)
@@ -58,5 +79,5 @@ module.exports = {
   createRecipe,
   getRecipeByID,
   updateRecipeByID,
-  
+  deleteRecipeByID
 };
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getAllRecipes, createRecipe, getRecipeByID, updateRecipeByID } = require('../controllers/recipeController');
+const { getAllRecipes, createRecipe, getRecipeByID, updateRecipeByID, deleteRecipeByID } = require('../controllers/recipeController');
 const recipesRouter = express.Router();
 
 recipesRouter.post('/', createRecipe);
 recipesRouter.get('/', getAllRecipes);
 recipesRouter.get('/:id', getRecipeByID);
-recipesRouter.put('/:id', updateRecipeByID)
+recipesRouter.put('/:id', updateRecipeByID);
+recipesRouter.delete('/:id', deleteRecipeByID)
 
 module.exports = recipesRouter;
