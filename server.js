948dba5b0ff2678f5e83f6252a9872dc16135a02
nathira99 <nathirafarveen99@gmail.com>
2025-10-02 15:36:35 +0530

    Ready for Render deployment

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀 on Render");
+});
+
+// Use Render's PORT
+const PORT = process.env.PORT || 5000;
+
 // connect to mongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -9,8 +16,8 @@ mongoose
     console.log("connected to mongoDB");
 
     // Listen on a port for incoming requests
-    app.listen(3002, "localhost", () => {
-      console.log("server listening on http://localhost:3002");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("server listening on port ${PORT}");
     });
   })
   .catch((err) => console.error("could not connect to mongoDB", err));
